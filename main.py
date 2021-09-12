9298783e5d78cfcf1f6013d2befd3590cba1c670
comhad <comhad@wearehackerone.com>
2021-09-12 19:00:04 +0100

    added youtube embed

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home() :
-    return open("static/homepage.html","r").read() # Not bothering with rendering, no variables
+    return open("templates/homepage.html","r").read() # Not bothering with rendering, no variables
+
+@app.route("/yt")
+def youtube() :
+    renderDict = {}
+    videoId = request.args.get("video", default="")
+    renderDict["id"] = videoId
+    soup = BeautifulSoup(requests.get("https://youtube.com/watch?v=" + videoId).text, 'html.parser')
+    splitTitle = soup.title.string.split("-")
+    splitTitle.pop()
+    renderDict["title"] = "-".join(splitTitle)
+    return render_template("embeds/youtube.html", renderDict=renderDict)
